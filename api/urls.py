14a3c77366a6714be5bbf51f2998ea338373f8b0
Pedro <plp750@gmail.com>
2021-11-01 22:48:49 +0100

    S02-30 URLs

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from django.conf.urls import include
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('new', admin.site.urls),
+    path('new', include(router.urls)),
 ]
