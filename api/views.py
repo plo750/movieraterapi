807c85ad887cfe11dcd176ee4c58c3f6bad09638
Pedro <plp750@gmail.com>
2021-11-08 23:15:25 +0100

    S03-35 Request Data

--- api/views.py
+++ api/views.py
@@ -10,11 +10,19 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    # https://127.0.0.1:8000/api/movies/[movie ID]]/rate_movie
+    # http://127.0.0.1:8000/api/movies/[movie ID]/rate_movie/
+    # in data => stars : '1-5'
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
-        response = {'message': 'its working'}
-        return Response(response, status=status.HTTP_200_OK)
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            print('movie title: ', movie.title)
+            response = {'message': 'its working'}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'you need to provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
