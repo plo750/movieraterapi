914e447f42dab3bd26eccfcaad0d102e1da1c1fb
Pedro Lopez <plp750@gmail.com>
2021-11-21 23:18:35 +0100

    S03-39 Register User

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 from .models import Movie, Rating
+from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        # Add a function defined in model
+        fields = ('id', 'username', 'password')
+        # will hide hashed password with get request
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
